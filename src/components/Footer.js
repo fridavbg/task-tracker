ac659cf7afa90d00d33013b6147645a95578f646
Frida Persson <fridavbg@gmail.com>
2021-01-26 13:11:27 +0100

    Ready for styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 const Footer = () => {
+	const location = useLocation();
 	return (
 		<footer>
 			<p>Copyright &copy; 2021</p>
-			<a href="/about">About</a>
+			{location.pathname === "/" && <Link to="/about">About</Link>}
 		</footer>
 	);
 };
